7118d163c16ab849befd3ad92344a986443b6584
Artem Kokobelyan <artyomkokobelyanwork@gmail.com>
2024-09-07 20:06:54 +0400

    hello world!

---
+++ src/main.java
@@ -0,0 +1,10 @@
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+import java.security.spec.RSAOtherPrimeInfo;
+
+public class main {
+
+    public static void main(String[] args) {
+
+        System.out.print("hello world");
+    }
+}
