5a8d0ab2189d2e2c526179029d295fbb6413c9e2
Artem Kokobelyan <artyomkokobelyanwork@gmail.com>
2024-09-13 18:38:42 +0400

    add words

--- src/SecondLecture/ShortWords.java
+++ src/SecondLecture/ShortWords.java
@@ -1,7 +1,24 @@
 package SecondLecture;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /*
 дана последовательность слов
 вывести вссе самые короткие слова
 */
 public class ShortWords {
+    public ShortWords (List<String> words) {
+        int minlen = words.get(0).length();
+        for (String word : words)
+            if (words.size()<  minlen)
+                minlen = words.size();
+
+        List<String> ans = new ArrayList<>();
+        for (String word : words)
+            if (word.length() == minlen)
+                ans.add(word + ' ');
+
+        System.out.println(ans);
+    }
 }
--- src/SecondLecture/Words.java
+++ src/SecondLecture/Words.java
@@ -5,17 +5,4 @@ import java.util.List;
 
 public class Words {
 
-    public Words (List<String> words) {
-        int minlen = words.get(0).length();
-        for (String word : words)
-            if (words.size()<  minlen)
-                minlen = words.size();
-
-        List<String> ans = new ArrayList<>();
-        for (String word : words)
-            if (word.length() == minlen)
-                ans.add(word + ' ');
-
-        System.out.println(ans);
-    }
 }
