7d6c19cc4ca09f69c155f7e9511937434a1bc66c
Artem Kokobelyan <artyomkokobelyanwork@gmail.com>
2024-09-25 17:44:51 +0400

    anuete payment

--- src/sixthLecture/CreditFind.java
+++ src/sixthLecture/CreditFind.java
@@ -13,6 +13,16 @@ public class CreditFind {
         double ySum = 1 + yearprocent / 100.0f;
         return Math.pow(mSum, 12) >= ySum;
     }
+
+    public boolean checkcredit(double mounthpayment, int period, double credirSum, double mounthProcent)
+    {
+        for (int i = 0; i < period; i++) {
+            double procPay = credirSum * (mounthpayment / 100.0f);
+            credirSum -= (mounthpayment - procPay);
+        }
+        return credirSum <=0;
+    }
+
     public double fbinsearch(int l, int r, double eps, double mounthprocent, double yearprocent) {
         while (l + eps < r) {
             int m = (l + r) / 2;
